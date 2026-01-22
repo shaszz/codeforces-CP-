a2fddca7c4601cc019790b825faffb69db859e78
Shashwath Nayak <159935998+shaszz@users.noreply.github.com>
2026-01-22 19:57:31 +0530

    Add initial implementation of source.cpp

---
+++ A.Bit++/source.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int x = 0;
+	cin >> n;
+	string arr[n];
+	for(int i = 0;i < n;i++) cin >> arr[i];
+	for(int i = 0;i < n;i++)  {
+		if(arr[i] == "X++" || arr[i] == "++X") {
+			x++;
+		} else {
+			x--;
+		}
+	} 
+
+	cout << x;
+
+	return 0;
+}
